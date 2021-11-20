47d2ccde9b092bc5234c6c605c58ef00b24873a9
deviran <iranmolina3@gmail.com>
2021-11-19 23:20:30 -0600

    my first model tbl_persona

--- appProgress/models.py
+++ appProgress/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class tbl_persona(models.Model):
+    pk_persona = models.AutoField(primary_key=True, null=False, blank=False)
+    primer_nombre = models.CharField('primer nombre', max_length=25, null=False, blank=False)
+    segundo_nombre = models.CharField('segundo nombre', max_length=25, null=True, blank=True)
+    primer_apellido = models.CharField('primer apellido', max_length=25, null=False, blank=False)
+    segundo_apellido = models.CharField('segundo apellido', max_length=25, null=True, blank=True)
+    cui = models.CharField('cui', max_length=25, null=False, blank=False)
+    fecha_nacimiento = models.DateField('fecha de nacimiento', null=False, blank=False)
+    direccion = models.TextField('adomicilio', null=False, blank=False)
+    telefono = models.CharField('telefono', max_length=8, null=False, blank=False)
+    estado = models.BooleanField('activo/inactivo', null=False, blank=False)
--- appProgress/views.py
+++ appProgress/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def singin(request):
+    return render(request, 'sing_in.html')
